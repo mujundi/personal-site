5983ef3f76abee1b745b5f562996b6f8c23f1052
Musa Jundi <musajundi@gmail.com>
2020-05-13 08:07:14 -0500

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Musa Jundi Portfolio",
+    author: "Musa Jundi",
+  },
+  plugins: ["gatsby-plugin-sass"],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <div>
+        <p>Here is a paragraph about me.</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default AboutPage
